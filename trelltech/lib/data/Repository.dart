5a6b7588921910d1cd1f15880c9cc921ba3fa6ca
Antoine Labard <antoine@labard.fr>
2024-03-19 12:12:51 +0100

    (repository) instantiate ListMethods in Repository.dart

--- trelltech/lib/data/Repository.dart
+++ trelltech/lib/data/Repository.dart
@@ -2,6 +2,7 @@ import 'package:trelltech/data/methods/CardMethods.dart';
 import 'package:trelltech/data/methods/MemberMethods.dart';
 
 import 'methods/BoardMethods.dart';
+import 'methods/ListMethods.dart';
 import 'methods/OrganizationMethods.dart';
 
 class Repository {
@@ -9,6 +10,7 @@ class Repository {
   static OrganizationMethods Organization = OrganizationMethods();
   static CardMethods Card = CardMethods();
   static MemberMethods Member = MemberMethods();
+  static ListMethods List = ListMethods();
 }
 
 Future<void> main() async {
