895fb1928a214e325126e0424699fc0c91a11c90
Antoine Labard <antoine@labard.fr>
2024-03-19 15:01:08 +0100

    (front) make the list of cards scrollable to show all cards and avoir overflow

--- trelltech/lib/widgets/ListWidget.dart
+++ trelltech/lib/widgets/ListWidget.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:trelltech/widgets/CardWidget.dart';
 
 import '../data/Repository.dart';
 import '../data/entities/CardEntity.dart';
 import '../data/entities/ListEntity.dart';
+
+/*
+create a list of CardWidget that can expand vertically to fit the content
+ */
 
 class ListWidget extends StatelessWidget {
   final ListEntity listEntity;
@@ -21,32 +24,34 @@ class ListWidget extends StatelessWidget {
         } else if (cardSnapshot.hasError) {
           return Text('Erreur de chargement des cartes');
         } else {
-          return Expanded(
-            child: Card(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ListTile(
-                    title: Text(listEntity.name!),
+          return Card(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                ListTile(
+                    title: Text(listEntity.name! +
+                        cardSnapshot.data!.length.toString() +
+                        " cards")),
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: ListView.builder(
+                      itemCount: cardSnapshot.data?.length ?? 0,
+                      shrinkWrap: true,
+                      // physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) =>
+                          CardWidget(cardEntity: cardSnapshot.data![index])),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Repository.List.getCards(listEntity.id!);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
                   ),
-                  Column(
-                    children: cardSnapshot.data?.map((card) {
-                          return CardWidget(cardEntity: card);
-                        }).toList() ??
-                        [],
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Repository.List.getCards(listEntity.id!);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                    ),
-                    child: Text('Add Card'),
-                  ),
-                ],
-              ),
+                  child: Text('Add Card'),
+                ),
+              ],
             ),
           );
         }
