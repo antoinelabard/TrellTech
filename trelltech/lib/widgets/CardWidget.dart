dfa3764ec3f52c38a0cacc97b1b62851b01d2534
Antoine Labard <antoine@labard.fr>
2024-03-20 07:21:30 +0100

    (front) add a LongPressDraggable to the CardWidget

--- trelltech/lib/widgets/CardWidget.dart
+++ trelltech/lib/widgets/CardWidget.dart
@@ -12,20 +12,25 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Text(cardEntity.name!),
-      ElevatedButton(
-        onPressed: () async {
-          await Repository.Card.delete(cardEntity);
-          deleteCallback();
-        },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.yellow,
-        ).copyWith(
-          minimumSize: MaterialStateProperty.all(Size(double.infinity, 48.0)),
-        ),
-        child: Text("Delete"),
-      )
-    ]);
+    return LongPressDraggable<CardEntity>(
+      data: cardEntity,
+      dragAnchorStrategy: pointerDragAnchorStrategy,
+      feedback: Text(cardEntity.name!),
+      child: Column(children: [
+        Text(cardEntity.name!),
+        ElevatedButton(
+          onPressed: () async {
+            await Repository.Card.delete(cardEntity);
+            deleteCallback();
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.yellow,
+          ).copyWith(
+            minimumSize: MaterialStateProperty.all(Size(double.infinity, 48.0)),
+          ),
+          child: Text("Delete"),
+        )
+      ])
+    );
   }
 }
